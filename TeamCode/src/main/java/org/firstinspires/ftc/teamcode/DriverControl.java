d6501cbc6ea6e69509ae83c0b214a0ef892034ea
Juan Santhosh <99568579+juan-santhosh@users.noreply.github.com>
2023-10-19 22:07:38 +1000

    Add files via upload

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControl.java
@@ -23,6 +23,7 @@ public class DriverControl extends LinearOpMode {
     Servo servoBaseLeft;
     Servo servoBaseRight;
 
+    double sliderPower = 0;
     double sliderPos = 0;
 
     double clawPos = 0;
@@ -111,30 +112,27 @@ public class DriverControl extends LinearOpMode {
 
         while (opModeIsActive()) {
             if (gamepad2.right_trigger > 0.1) {
-                if (sliderPos < MAX_SLIDER) { sliderPos += gamepad2.right_trigger * 0.001; }
-
-                motorSliderLeft.setTargetPosition((int) (-sliderPos));
-                motorSliderRight.setTargetPosition((int) (sliderPos));
-
-                motorSliderLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                motorSliderRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-                motorSliderLeft.setPower(gamepad2.right_trigger);
-                motorSliderRight.setPower(gamepad2.right_trigger);
-
+                if (sliderPos < MAX_SLIDER) {
+                    sliderPos += gamepad2.right_trigger * 0.001;
+                }
+                sliderPower = gamepad2.right_trigger;
             } else if (gamepad2.left_trigger > 0.1) {
-                if (sliderPos > MIN_SLIDER) { sliderPos -= gamepad2.left_trigger * 0.001; }
-
-                motorSliderLeft.setTargetPosition((int) (-sliderPos));
-                motorSliderRight.setTargetPosition((int) (sliderPos));
-
-                motorSliderLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                motorSliderRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-                motorSliderLeft.setPower(gamepad2.left_trigger);
-                motorSliderRight.setPower(gamepad2.left_trigger);
-
+                if (sliderPos > MIN_SLIDER) {
+                    sliderPos -= gamepad2.left_trigger * 0.001;
+                }
+                sliderPower = gamepad2.left_trigger;
+            } else {
+                sliderPower = 0.1;
             }
+
+            motorSliderLeft.setTargetPosition((int) (-sliderPos));
+            motorSliderRight.setTargetPosition((int) (sliderPos));
+
+            motorSliderLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            motorSliderRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            motorSliderLeft.setPower(sliderPower);
+            motorSliderRight.setPower(sliderPower);
 
             telemetry.addData("Desired L", motorSliderLeft.getTargetPosition());
             telemetry.addData("Actual L", motorSliderLeft.getCurrentPosition());
