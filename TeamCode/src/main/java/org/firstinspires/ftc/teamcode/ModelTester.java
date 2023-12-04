2794e4d78b97b809b1b69694cc0edda2950a91fc
DW09877 <danielwon877@gmail.com>
2023-12-05 00:51:28 +1000

    Added a pre-trained (ONNX) model. Implemented the ModelTester op mode.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ModelTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ModelTester.java
@@ -8,28 +8,32 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.vision.VisionPortal;
 
-import java.io.File;
 import java.util.Locale;
 
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Scalar;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.core.Mat;
-import org.opencv.imgproc.Imgproc;
 
-import org.tensorflow.lite.*;
-
+import org.opencv.dnn.Dnn;
+import org.opencv.dnn.Net;
 
 @TeleOp(name = "Camera Frame Capture", group = "Utility")
 public class ModelTester extends LinearOpMode {
     boolean lastX;
     long capReqTime;
+    double left_prob, right_prob, center_prob;
 
     @Override
     public void runOpMode() {
         VisionPortal portal;
-        InterpreterApi model = InterpreterApi.create(new File("models\\propPosDetectorM4.tflite"), new InterpreterApi.Options());
+
+        Net model = Dnn.readNetFromONNX("propPosDetector.onnx");
+
         portal = new VisionPortal.Builder()
                 .setCamera(hardwareMap.get(WebcamName.class, "FrontalCamera"))
-                .setCameraResolution(new Size(192, 72))
+                .setCameraResolution(new Size(256, 144))
                 .build();
 
         while (!isStopRequested()) {
@@ -38,10 +42,28 @@ public class ModelTester extends LinearOpMode {
             if (x && !lastX) {
                 portal.saveNextFrameRaw("inputImage.png");
                 Mat inputImage = Imgcodecs.imread("inputImage.png");
-                Mat inputImageGrayscale = new Mat();
-                Imgproc.cvtColor(inputImage, inputImageGrayscale, Imgproc.COLOR_RGB2GRAY);
+                Mat extractedChannel = new Mat();
+                // coi (below) should be 2 if red and 0 if blue
+                Core.extractChannel(inputImage, extractedChannel, 2);
+                Mat fullBlack = new Mat();
+                Core.multiply(Mat.ones(144, 256, CvType.CV_8UC1), new Scalar(255), fullBlack);
+                Core.subtract(fullBlack, extractedChannel, extractedChannel);
+                for (int i = 0; i < 144; i++) {
+                    for (int j = 0; j < 256; j++) {
+                        if (extractedChannel.get(i, j)[0] > 30) {
+                            extractedChannel.put(i, j, 255);
+                        }
+                    }
+                }
+                Mat inputFloatImage = new Mat(144, 256, CvType.CV_32FC1);
+                extractedChannel.convertTo(inputFloatImage, CvType.CV_32FC1);
+                Mat inputBlob = Dnn.blobFromImage(extractedChannel, 1.0, new org.opencv.core.Size(144, 256));
 
-
+                model.setInput(inputBlob);
+                Mat y = model.forward();
+                left_prob = y.get(0, 0)[0];
+                right_prob = y.get(0, 0)[0];
+                center_prob = y.get(0, 0)[0];
 
                 capReqTime = System.currentTimeMillis();
             }
@@ -55,6 +77,16 @@ public class ModelTester extends LinearOpMode {
 
             if (capReqTime != 0) {
                 telemetry.addLine("\nCaptured Frame.");
+                telemetry.addLine("\nLeft Probability: " + left_prob);
+                telemetry.addLine("Right Probability: " + right_prob);
+                telemetry.addLine("Centre Probability: " + center_prob);
+                if (left_prob > center_prob && left_prob > right_prob) {
+                    telemetry.addLine("\nConclusion: Left");
+                } else if (right_prob > center_prob && right_prob > left_prob) {
+                    telemetry.addLine("\nConclusion: Right");
+                } else if (center_prob > right_prob && center_prob > left_prob) {
+                    telemetry.addLine("\nConclusion: Centre");
+                }
             }
 
             if (capReqTime != 0 && System.currentTimeMillis() - capReqTime > 1000) {
