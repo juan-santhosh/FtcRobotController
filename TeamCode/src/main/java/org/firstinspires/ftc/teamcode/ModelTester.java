bf0c01b8db2e27e86cbd7553cb1305901c9083fb
DW09877 <danielwon877@gmail.com>
2023-12-09 11:01:38 +1000

    Created an "asset" folder and moved the ONNX model file to there.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ModelTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ModelTester.java
@@ -14,6 +14,7 @@ import org.opencv.core.Mat;
 import org.opencv.dnn.Dnn;
 import org.opencv.dnn.Net;
 
+
 @TeleOp(name = "Model Tester", group = "Utility")
 public class ModelTester extends LinearOpMode {
     boolean lastX;
